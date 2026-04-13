26e67c68d9abdba2ea3931163400177e99856c5d
hmouis <houdayfamouis@gmail.com>
2026-04-13 18:29:50 +0100

    Add: Converting methods

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -9,8 +9,13 @@
 
 class ScalarConverter
 {
-    public:
+    private:
+        bool ConvertToInt(std::string &str);
+        bool ConvertToChar(std::string &str);
+        bool ConvertToFloat(std::string &str);
+        bool ConvertToDouble(std::string &str);
 
+    public:
         ScalarConverter();
         ScalarConverter(const ScalarConverter &other);
         ScalarConverter operator=(const ScalarConverter &other);
